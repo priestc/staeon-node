55c596ba28a12974433f5e51be1f67aeee9999ac
Chris Priest <cp368202@ohiou.edu>
2019-02-17 17:45:26 -0700

    renamed stayon app to 'main'

--- stayon_node/stayon_node/urls.py
+++ stayon_node/stayon_node/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^stayon/', include("stayon.urls"))
+    url(r'^stayon/', include("main.urls"))
 ]
